e0a6e69d5070961563d2c86504172adb5da509e4
test <conyasetia@gmail.com>
2024-03-05 14:17:01 +0300

    remove vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import {Analytics} from '@vercel/analytics/react';
 
 const font = Poppins({ subsets: ["latin"],weight:'400' });
 
@@ -18,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>{children}</body>
-      <Analytics/>
+     
     </html>
   );
 }
